91034038d29f7fad78e8fa52244a47b1c02a4522
leo <leoperez3006@gmail.com>
2023-05-01 13:34:11 +0900

    counter

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    <div>
+    <h4>
+      Current count: 
+    </h4>
+    <h1>{{ count }}</h1>
+    <button @click="count--">-</button>
+    <button @click="count++">+</button>
+  </div>
+  </main>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const count = ref(0);
+</script>
+
+<style scoped>
+main{
+  background-color: rgb(63, 63, 63);
+  height: 100vh;
+  width: 100vw;
+  color: rgb(228, 228, 228);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+}
+div{
+  text-align: center;
+}
+
+button{
+  margin: 5%;
+  width: 30px;
+
+}
+</style>
